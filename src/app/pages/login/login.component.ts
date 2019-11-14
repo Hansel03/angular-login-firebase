dc0ab595a2090d4fdb843cbd8dead6e07a549274
Hansel <hanselcapillo@hotmail.com>
2019-11-13 22:41:00 -0400

    validacion de formulario de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { LoginModel } from "src/app/models/login.model";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
+  login: LoginModel;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.login = new LoginModel();
   }
 
+  public onSubmit(loginForm: NgForm) {
+    if (loginForm.invalid) {
+      return;
+    }
+    console.log(loginForm);
+  }
 }
